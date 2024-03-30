cdff28e7a84fb22f7283a320d83b704e419c91cd
Alec Roberson <alectroberson@gmail.com>
2024-03-30 11:01:48 -0700

    working changes to anim.py and geom.py

--- animations/anim.py
+++ animations/anim.py
@@ -4,11 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from geom import *
 
-
 mn.config.media_width = "75%"
 mn.config.verbosity = "WARNING"
-
-
 
 class gatepot(mn.ThreeDScene):
     def construct(self):
@@ -99,7 +96,6 @@ class gatepot(mn.ThreeDScene):
             
             self.play(mn.MoveToTarget(surf), run_time=2)
             self.wait(1.5)
-        
 
 class tunneling(mn.ThreeDScene):
     def construct(self):
@@ -223,160 +219,155 @@ class tunneling(mn.ThreeDScene):
         '''
         pass
 
-
 class fun_tunneling(mn.ThreeDScene):
     def construct(self):
-        g = Dots()
-        # g.add_dot('P0', -160e-9, 95e-9, 20e-9, -1e-3)
-        # g.add_dot('P1', 0, 0, 50e-9, 1e-3)
-        # g.add_dot('P2', 160e-9, -95e-9, 20e-9, -1e-3)
+        # initialize the dots
+        dots = Dots()
 
-        WELL = -1e-3
-        SOFT_WELL = -0.2e-3
+        # parameters
+        WELL = -5e-4
+        M_WELL = -1e-3
+        SOURCE_WELL = -3e-4
+        DRAIN_WELL = -7e-4
+        SOFT_WELL = -5e-5
         BARRIER = 1e-4
-        g.add_alternating(
+        Y_OFFSET = 95e-9
+        P_RAD = 135E-9
+        X_RAD = 90E-9
+        SPACING = -30E-9
+        M_RAD = 135E-9
+
+        # alternating quantum dots
+        dots_left_bound, dots_right_bound = dots.add_alternating(
             n=3,
-            p_rad=90e-9,
-            x_rad=135e-9,
-            spacing=5e-9,
+            p_rad=P_RAD,
+            x_rad=X_RAD,
+            spacing=SPACING,
             p_h=WELL,
             x_h=BARRIER,
-            y_offset=95e-9)
+            y_offset=-Y_OFFSET)
         
-        # add axes
+        # add baths
+        source_bath = SquareBath(dots_left_bound, -Y_OFFSET, 'DL', 1e-8, SOURCE_WELL)
+        drain_bath = SquareBath(dots_right_bound, -Y_OFFSET, 'DR', 1e-8, DRAIN_WELL)
+
+        # add measure dot
+        left_bound, right_bound = dots.add_alternating(
+            n=1,
+            p_rad=M_RAD,
+            x_rad=X_RAD,
+            spacing=SPACING,
+            p_h=M_WELL,
+            x_h=BARRIER,
+            y_offset=Y_OFFSET,
+            prefixes='MZ')
+        
+        # add baths
+        m_drain_bath = SquareBath(left_bound, Y_OFFSET, 'UL', 1e-7, DRAIN_WELL)
+        m_source_bath = SquareBath(right_bound, Y_OFFSET, 'UR', 1e-7, SOURCE_WELL)
+        
+        # initialize the combined geometry
+        geom = CombinedGeometry(dots, drain_bath, source_bath, m_drain_bath, m_source_bath)
+
+        # initialize axes
         ax = mn.ThreeDAxes(
             x_range=(-400e-9, 400e-9, 100e-9), 
             y_range=(-200e-9, 200e-9, 100e-9), 
             z_range=(-1.5e-3, 1.5e-3, 1e-4),
             axis_config={'include_ticks': True},
             tips=False)
-        self.add(ax)
+        # self.add(ax) # add the axes
 
-        # add surface plot
+        # plot the potential as a surface
         surf = ax.plot_surface(
-            function=g,
+            function=geom,
             u_range=(-400e-9, 400e-9),
             v_range=(-300e-9, 300e-9),
             fill_opacity=0.3)
         self.add(surf)
 
-        # move camera
+        # set the camera position
         # self.set_camera_orientation(phi=60*mn.DEGREES, theta=60*mn.DEGREES, zoom=0.75)
-        self.set_camera_orientation(phi=60*mn.DEGREES, theta=80*mn.DEGREES, zoom=0.75)
+        self.set_camera_orientation(phi=60*mn.DEGREES, theta=(180+80)*mn.DEGREES, zoom=0.75)
 
+        start_pos = ax.coords_to_point(dots_left_bound-30e-9, -Y_OFFSET, 0)
         # add electrons!
-        e0 = mn.Sphere(
-            center = ax.coords_to_point(*g.get_pos('P0'), 0), radius=0.2)
-        e1 = mn.Sphere(
-            center = ax.coords_to_point(*g.get_pos('P1'), 0), radius=0.2)
-        self.add(e0, e1)
+        e0 = mn.Dot(point=start_pos, radius=0.1)
+        # e0 = mn.Dot(
+        #     point = ax.coords_to_point(*dots.get_pos('P0'), 0), radius=0.2)
+        # e1 = mn.Dot(
+        #     point = ax.coords_to_point(*dots.get_pos('P1'), 0), radius=0.2)
+        # self.add(e0) # , e1)
 
         # wait for a second
         self.wait(1)
-        
+
         # targets for interaction animation
-        surf.save_state()
-        g.edit_height('P0', SOFT_WELL)
-        g.edit_height('X1', WELL)
-        g.edit_height('P1', SOFT_WELL)
+        # dots.edit_height('P0', SOFT_WELL)
+        
+        # e0.generate_target()
+        # e0.target.move_to(ax.coords_to_point(*dots.get_pos('X0'),0))
 
-        e0.save_state()
-        e1.save_state()
+        surf.save_state()
+        dots.edit_height('X0', SOURCE_WELL)
+        surf.target = ax.plot_surface(
+            function=geom,
+            u_range=(-400e-9, 400e-9),
+            v_range=(-300e-9, 300e-9),
+            fill_opacity=0.3)
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+        surf.save_state()
+        dots.edit_height('P0', SOFT_WELL)
+        dots.edit_height('X1', WELL)
+        dots.edit_height('P1', SOFT_WELL)
+        surf.target = ax.plot_surface(
+            function=geom,
+            u_range=(-400e-9, 400e-9),
+            v_range=(-300e-9, 300e-9),
+            fill_opacity=0.3)
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+
+        surf.save_state()
+        dots.edit_height('P1', SOFT_WELL)
+        dots.edit_height('X2', WELL)
+        dots.edit_height('P2', SOFT_WELL)
+        surf.target = ax.plot_surface(
+            function=geom,
+            u_range=(-400e-9, 400e-9),
+            v_range=(-300e-9, 300e-9),
+            fill_opacity=0.3)
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+        surf.save_state()
+        dots.edit_height('Z0', WELL)
+        dots.edit_height('M0', SOFT_WELL)
+        surf.target = ax.plot_surface(
+            function=geom,
+            u_range=(-400e-9, 400e-9),
+            v_range=(-300e-9, 300e-9),
+            fill_opacity=0.3)
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+        '''
+        self.play(mn.MoveToTarget(e0, rate_func=mn.rate_functions.ease_in_sine), mn.MoveToTarget(surf, rate_func=mn.rate_functions.ease_in_sine), run_time=2)
+
         e0.generate_target()
-        e1.generate_target()
-        x, y, _ = ax.coords_to_point(*g.get_pos('X1'),0)
-        e0.target.move_to(np.array([x-0.3, y, 0]))
-        e1.target.move_to(np.array([x+0.3, y, 0]))
+        e0.target.move_to(ax.coords_to_point(*dots.get_pos('P0'),0))
 
-        surf.target = ax.plot_surface(
-            function=g,
-            u_range=(-400e-9, 400e-9),
-            v_range=(-300e-9, 300e-9),
-            fill_opacity=0.3)
-        
-        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)),
-            mn.Succession(mn.MoveToTarget(e0), mn.Restore(e0)),
-            mn.Succession(mn.MoveToTarget(e1), mn.Restore(e1)), run_time=2)
+        self.play(mn.MoveToTarget(e0, rate_func=mn.rate_functions.ease_out_sine), mn.Restore(surf, rate_func=mn.rate_functions.ease_out_sine), run_time=2)
+
+        '''
+        # self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)),
+        #     mn.Succession(mn.MoveToTarget(e0), mn.Restore(e0)), run_time=2)
+            # mn.Succession(mn.MoveToTarget(e1), mn.Restore(e1)), run_time=2)
 
         self.wait(2)
-
-        '''
-        # create electron
-        electron = mn.Sphere(
-            center=ax.coords_to_point(-375e-9, 95e-9, 20e-6),
-            radius=0.2,
-            color=mn.RED)
-        self.add(electron)
-        self.wait(1)
-
-        # create electron target in P0
-        electron.generate_target()
-        electron.target.move_to(ax.coords_to_point(-160e-9, 95e-9, -300e-6))
-        # create surface target while electron moves
-        surf.save_state()
-        g['X0'].set_voltage(-1.5e-3)
-        surf.target = ax.plot_surface(
-            function=g,
-            u_range=(-400e-9, 400e-9),
-            v_range=(-300e-9, 300e-9),
-            fill_opacity=0.3)
-        g['X0'].set_voltage(1.5e-3)
-        self.play(
-            mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)),
-            mn.MoveToTarget(electron))
-        
-        self.wait(2)
-
-        # create electron target in P1
-        electron.generate_target()
-        electron.target.move_to(ax.coords_to_point(0, 95e-9, -300e-6))
-        # create surface target while electron moves
-        surf.save_state()
-        g['X1'].set_voltage(-1.5e-3)
-        surf.target = ax.plot_surface(
-            function=g,
-            u_range=(-400e-9, 400e-9),
-            v_range=(-300e-9, 300e-9),
-            fill_opacity=0.3)
-        g['X1'].set_voltage(1.5e-3)
-        self.play(
-            mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)),
-            mn.MoveToTarget(electron))
-        
-        self.wait(2)
-
-        # create electron target in P1
-        electron.generate_target()
-        electron.target.move_to(ax.coords_to_point(160e-9, 95e-9, -300e-6))
-        # create surface target while electron moves
-        surf.save_state()
-        g['X2'].set_voltage(-1.5e-3)
-        surf.target = ax.plot_surface(
-            function=g,
-            u_range=(-400e-9, 400e-9),
-            v_range=(-300e-9, 300e-9),
-            fill_opacity=0.3)
-        g['X2'].set_voltage(1.5e-3)
-        self.play(
-            mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)),
-            mn.MoveToTarget(electron))
-        
-        '''
-        
-        '''
-        # wiggle gate voltages
-        for name, v in [('X0', -1e-3), ('X0', 1.5e-3), ('X1', -1e-3), ('X1', 1.5e-3)]:
-            g[name].set_voltage(v)
-            surf.target = ax.plot_surface(
-                function=g,
-                u_range=(-400e-9, 400e-9),
-                v_range=(-300e-9, 300e-9),
-                fill_opacity=0.3)
-            
-            self.play(mn.MoveToTarget(surf), run_time=2)
-            self.wait(1.5)
-        '''
-        pass
 
 class testing(mn.ThreeDScene):
     def construct(self):
@@ -412,7 +403,6 @@ class testing(mn.ThreeDScene):
         # self.play(mn.Create(labels))
         self.wait(2)
 
-
 class bath_test(mn.ThreeDScene):
     def construct(self):
         g = SquareBath(np.array([0,0]), 'UR', 1e-7, -1e-3)
@@ -442,3 +432,213 @@ class bath_test(mn.ThreeDScene):
 
         # wait for a second
         self.wait(1)
+
+class new_tunneling(mn.ThreeDScene):
+    def construct(self):
+        # initialize the dots
+        dots = Dots()
+
+        # parameters
+        WELL = -8e-4
+        BARRIER = 4e-4
+        P_RAD = 135E-9
+        X_RAD = 90E-9
+
+        # alternating quantum dots
+        dots = DotSeries(3, P_RAD, X_RAD, WELL, BARRIER, P_RAD/2, X_RAD/2, 0)
+        
+        # initialize axes
+        ax = mn.ThreeDAxes(
+            x_range=(-8e-7, 8e-7, 1e-7), 
+            y_range=(-2e-7, 2e-7, 1e-7), 
+            z_range=(-1.5e-3, 1.5e-3, 1e-4),
+            axis_config={'include_ticks': True},
+            tips=False)
+        # self.add(ax) # add the axes
+
+        # plot the potential as a surface
+        surf = ax.plot_surface(
+            function=dots,
+            u_range=(-8e-7, 8e-7),
+            v_range=(-6e-7, 6e-7),
+            fill_opacity=0.3)
+        self.add(surf)
+
+        # set the camera position
+        # self.set_camera_orientation(phi=60*mn.DEGREES, theta=60*mn.DEGREES, zoom=0.75)
+        self.set_camera_orientation(phi=60*mn.DEGREES, theta=(180+80)*mn.DEGREES, zoom=0.75)
+
+        # wait for a second
+        self.wait(1)
+
+        # targets for interaction animation
+        dots.tunnel_across('X1')
+        surf.save_state()
+        surf.target = ax.plot_surface(
+            function=dots,
+            u_range=(-8e-7, 8e-7),
+            v_range=(-6e-7, 6e-7),
+            fill_opacity=0.3)
+        dots.reset()
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+        dots.tunnel_across('X2')
+        surf.save_state()
+        surf.target = ax.plot_surface(
+            function=dots,
+            u_range=(-8e-7, 8e-7),
+            v_range=(-6e-7, 6e-7),
+            fill_opacity=0.3)
+        dots.reset()
+
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+        
+        
+        '''
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+
+        surf.save_state()
+        dots.edit_height('P1', SOFT_WELL)
+        dots.edit_height('X2', WELL)
+        dots.edit_height('P2', SOFT_WELL)
+        surf.target = ax.plot_surface(
+            function=geom,
+            u_range=(-400e-9, 400e-9),
+            v_range=(-300e-9, 300e-9),
+            fill_opacity=0.3)
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+        surf.save_state()
+        dots.edit_height('Z0', WELL)
+        dots.edit_height('M0', SOFT_WELL)
+        surf.target = ax.plot_surface(
+            function=geom,
+            u_range=(-400e-9, 400e-9),
+            v_range=(-300e-9, 300e-9),
+            fill_opacity=0.3)
+        
+        self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
+
+        self.play(mn.MoveToTarget(e0, rate_func=mn.rate_functions.ease_in_sine), mn.MoveToTarget(surf, rate_func=mn.rate_functions.ease_in_sine), run_time=2)
+
+        e0.generate_target()
+        e0.target.move_to(ax.coords_to_point(*dots.get_pos('P0'),0))
+
+        self.play(mn.MoveToTarget(e0, rate_func=mn.rate_functions.ease_out_sine), mn.Restore(surf, rate_func=mn.rate_functions.ease_out_sine), run_time=2)
+
+        '''
+        # self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)),
+        #     mn.Succession(mn.MoveToTarget(e0), mn.Restore(e0)), run_time=2)
+            # mn.Succession(mn.MoveToTarget(e1), mn.Restore(e1)), run_time=2)
+
+        self.wait(2)
+
+def state(dots):
+    def func(x):
+        DOTS = np.array(dots)
+        SIG = 0.5
+        DEP = -2
+
+        d = DOTS - x
+        return np.sum(DEP * np.exp(-0.5*(d/SIG)**2))
+    return func
+
+class tunneling2D(mn.Scene):
+    def construct(self):
+
+        # parameters
+        # WELL = -8e-4
+        # BARRIER = 4e-4
+        # P_RAD = 135E-9
+        # X_RAD = 90E-9
+
+        # alternating quantum dots
+        # dots = DotSeries(3, P_RAD, X_RAD, WELL, BARRIER, P_RAD/2, X_RAD/2, 0)
+        
+        # initialize axes
+        # ax = mn.Axes(
+        #     x_range=(-8e-7, 8e-7, 1e-7), 
+        #     y_range=(-1.5e-3, 1.5e-3, 1e-4),
+        #     axis_config={'include_ticks': True},
+        #     tips=False)
+        ax = mn.Axes().add_coordinates()
+        # self.add(ax) # add the axes
+
+        # plot the potential as a surface
+        # surf = ax.plot_surface(
+        #     function=dots,
+        #     u_range=(-8e-7, 8e-7),
+        #     v_range=(-6e-7, 6e-7),
+        #     fill_opacity=0.3)
+        # self.add(surf
+
+        e1 = mn.Dot(ax.coords_to_point(-3,0,0), color=mn.RED)
+        e2 = mn.Dot(ax.coords_to_point(0,0,0), color=mn.RED)
+        e3 = mn.Dot(ax.coords_to_point(3,0,0), color=mn.RED)
+
+        pot = ax.plot(state([-3,0,3]), (-6,6))
+        self.play(mn.Create(pot), mn.Create(e1), mn.Create(e2), mn.Create(e3))
+        
+        # wait for a second
+        self.wait(1)
+
+        # targets for interaction animation
+        pot.save_state()
+        pot.target = ax.plot(state([-1.5,3]), (-6,6))
+        e1.save_state()
+        e1.generate_target()
+        e1.target.move_to(ax.coords_to_point(-1.7,0,0))
+        e2.save_state()
+        e2.generate_target()
+        e2.target.move_to(ax.coords_to_point(-1.3,0,0))
+
+        # play movement
+        self.play(
+            mn.MoveToTarget(pot),
+            mn.MoveToTarget(e1),
+            mn.MoveToTarget(e2), run_time=0.5)
+        self.wait(1)
+        # play reset
+        self.play(
+            mn.Restore(pot),
+            mn.Restore(e1),
+            mn.Restore(e2), run_time=0.5)
+        self.wait(1)
+
+        # prepare for second interaction animation
+        pot.save_state()
+        pot.target = ax.plot(state([-3, 1.5]), (-6,6))
+        e2.save_state()
+        e2.generate_target()
+        e2.target.move_to(ax.coords_to_point(1.3,0,0))
+        e3.save_state()
+        e3.generate_target()
+        e3.target.move_to(ax.coords_to_point(1.7,0,0))
+
+        # play movement
+        self.play(
+            mn.MoveToTarget(pot),
+            mn.MoveToTarget(e2),
+            mn.MoveToTarget(e3), run_time=0.5)
+        self.wait(1)
+        # play reset
+        self.play(
+            mn.Restore(pot),
+            mn.Restore(e2),
+            mn.Restore(e3), run_time=0.5)
+        self.wait(1)
+
+
+        # dots.tunnel_across('X2')
+        # surf.save_state()
+        # surf.target = ax.plot_surface(
+        #     function=dots,
+        #     u_range=(-8e-7, 8e-7),
+        #     v_range=(-6e-7, 6e-7),
+        #     fill_opacity=0.3)
+        # dots.reset()
+
+        # self.play(mn.Succession(mn.MoveToTarget(surf), mn.Restore(surf)), run_time=2)
--- animations/geom.py
+++ animations/geom.py
@@ -1,4 +1,73 @@
 import numpy as np
+import manim as mn
+
+class DotSeries:
+    def __init__(self, n:int, a_rad:float, b_rad:float, a_h:float, b_h:float, a_sig:float, b_sig:float, y_offset:float=0):
+        # calculate total width
+        self.width = n*2*a_rad + (n+1)*2*b_rad
+
+        # store the initialization parameters
+        self.n = n
+        self.a_sig, self.b_sig = a_sig, b_sig
+        self.a_rad, self.b_rad = a_rad, b_rad
+        self.a_h, self.b_h = a_h, b_h
+        self.y_offset = y_offset
+
+        # create arrays
+        self.centers = np.zeros((2*n+1,2))
+        self.sigs = np.zeros((2*n+1,))
+        self.heights = np.zeros((2*n+1,))
+
+        # set all the parameters to base values
+        self.reset()
+
+    @property
+    def x_min(self):
+        return -self.width/2
+    
+    @property
+    def x_max(self):
+        return self.width/2
+    
+    def reset(self):
+        # create a list of center points for each dot
+        # these are evenly spaced so it's fairly simple
+        self.centers[:,0] = [self.x_min + self.b_rad + i*(self.a_rad+self.b_rad) for i in range(2*self.n+1)]
+        self.centers[:,1] = self.y_offset
+
+        # list of sigmas for each dot --  these are alternating
+        self.sigs[0::2] = self.b_sig
+        self.sigs[1::2] = self.a_sig
+
+        # heights for each dot -- also just alternating
+        self.heights[0::2] = self.b_h
+        self.heights[1::2] = self.a_h
+
+    def __call__(self, x, y):
+        # turn the input into a vector
+        r = np.array([x,y])
+        # calculate distances to each of the dots
+        d = np.linalg.norm(self.centers - r, axis=1)
+        # calculate a sum of gaussian distributions from each dot
+        return np.sum(self.heights * np.exp(-0.5*(d/self.sigs)**2))
+
+    def get_index(self, dot:str):
+        return 2 * int(dot[1]) + (dot[0] == 'A' or dot[0] == 'P')
+    
+    def tunnel_across(self, dot):
+        # make sure we are trying to tunnel across a barrier
+        if dot[0] != 'B' and dot[0] != 'X':
+            raise ValueError('Dot name must start with "B" or "X"')
+        # get the index of the dot to tunnel across
+        i = self.get_index(dot)
+        # move previous dot a bit forward
+        if i != 0:
+            self.centers[i-1,0] = self.centers[i,0] - self.a_sig
+        # move the next dot a bit back
+        if i != 2*self.n:
+            self.centers[i+1,0] = self.centers[i,0] + self.a_sig
+        # drop the barrier
+        self.heights[i] = 0
 
 class Dots:
     def __init__(self, names:'list[str]'=[], positions:np.ndarray=np.array([]).reshape(0,2), radii:np.ndarray=np.array([]).reshape(0,1), heights:np.ndarray=np.array([]).reshape(0,1)):
@@ -34,7 +103,7 @@ class Dots:
         i = self.names.index(name)
         self.radii[i] = new_radius
 
-    def add_alternating(self, n:int, p_rad:float, x_rad:float, spacing:float, p_h:float=0, x_h:float=0, y_offset:float=0) -> None:
+    def add_alternating(self, n:int, p_rad:float, x_rad:float, spacing:float, p_h:float=0, x_h:float=0, y_offset:float=0, prefixes='PX') -> None:
         ''' Add alternating gates to the geometry.
         
         Parameters
@@ -59,16 +128,17 @@ class Dots:
         tuple[float, float]
             The bounding x and y coordinates of the entire geometry (including a gap on either side).
         '''
+        p_pfx, x_pfx = prefixes
         # compute total width
         total_width = 2*n*p_rad + (n+1)*spacing
         # add p dots
         for i in range(n):
             x = -total_width/2 + spacing + p_rad + i*(2*p_rad + spacing)
-            self.add_dot(f'P{i}', x, y_offset, p_rad, p_h)
+            self.add_dot(f'{p_pfx}{i}', x, y_offset, p_rad, p_h)
         # add x dots
         for i in range(n+1):
             x = -total_width/2 + spacing/2 + i*(2*p_rad + spacing)
-            self.add_dot(f'X{i}', x, y_offset, x_rad, x_h)
+            self.add_dot(f'{x_pfx}{i}', x, y_offset, x_rad, x_h)
         # return bounding coordinates
         return (-total_width/2, total_width/2)
 
@@ -77,8 +147,8 @@ class Dots:
         return self.positions[i]
 
 class SquareBath:
-    def __init__(self, corner:np.ndarray, bounding:str, width:float=1, height:float=0):
-        self._x = corner
+    def __init__(self, x, y, bounding:str, width:float=1, height:float=0):
+        self._x = np.array([x,y])
         self._height = height
         self._width = width
         # set sign array
@@ -96,4 +166,46 @@ class SquareBath:
     def __call__(self, x, y) -> float:
         s = np.array([x,y]).reshape(2,) - self._x
         return self._height/(1 + np.sum(np.exp(self._signs * s * 5 / self._width)))
-        
+
+class Electron(mn.Dot):
+    def __init__(self, pos:np.ndarray, radius:float=0.05):
+        super().__init__(pos, radius=radius)
+    
+    @property
+    def pos(self) -> np.ndarray:
+        return self.get_center()
+    
+    def distance_to(self, other:'Electron') -> float:
+        return np.linalg.norm(self.pos - other.pos)
+    
+    def distance_to_point(self, other:np.ndarray) -> float:
+        return np.linalg.norm(self.pos - other)
+
+class ElectronGroup:
+    def __init__(self, *electrons):
+        self.electrons = list(electrons)
+    
+    def add(self, electron:'Electron') -> None:
+        self.electrons.append(electron)
+    
+    def closest_electron(self, pos:np.ndarray) -> 'tuple[Electron,float]':
+        return min([(e, e.distance_to_point(pos)) for e in self.electrons], key=lambda x: x[1])
+    
+    def add_bunch(self, center:np.ndarray, n:int, sample_radius:float, electron_radius:float) -> None:
+        # loop to randomly add electrons
+        for _ in range(n):
+            angle = 2*np.pi*np.random.rand()
+            rad = np.random.rand() * sample_radius
+            pos = center + rad*np.array([np.cos(angle), np.sin(angle)])
+
+            self.add(Electron(pos, electron_radius))
+
+class CombinedGeometry:
+    def __init__(self, *geoms):
+        self._geoms = list(geoms)
+    
+    def add(self, g):
+        self._geoms.append(g)
+    
+    def __call__(self, x:float, y:float) -> float:
+        return sum([g(x,y) for g in self._geoms])
